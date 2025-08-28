cb31e9dce7f080d919b337504f9507f6db0b6f8c
Dalton Nguyen <dalton2144dn@gmail.com>
2025-08-28 10:39:54 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Dalton Nguyen
+ * Date: 2025/08/28
+ *
+ *
+ * Prints me and my partners name and major
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+printf("Hello World, My name is Dalton!\n");
+printf("My major is Computer Science!\n");
+
+return 0;
+}
